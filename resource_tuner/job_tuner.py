d0bd1cfbe08ba3bc6613580b443790bb2f64867c
Evan Wang <evan.l.wang@vanderbilt.edu>
2021-04-02 03:43:11 +0000

    Increment core count for job tuning

--- resource_tuner/job_tuner.py
+++ resource_tuner/job_tuner.py
@@ -9,15 +9,15 @@ config.load_kube_config()
 api_instance = client.BatchV1Api()
 
 
-def create_job_object():
+def create_job_object(iteration):
     metadata = client.V1ObjectMeta(
         namespace=DEFAULT_NAMESPACE, name=JOB_NAME)
 
-    resource_requests = {"cpu": "500m"}
+    resource_requests = {"cpu": f"{200 + 100 * iteration}m"}
     resource_requirements = client.V1ResourceRequirements(
         requests=resource_requests, limits=resource_requests)
     container = client.V1Container(
-        name="matmul", image="evanw1999/matmul:public")
+        name="matmul", image="evanw1999/matmul:public", image_pull_policy="Always")
     container.resources = resource_requirements
     spec = client.V1PodSpec(
         containers=[container], restart_policy="Never", scheduler_name="kube-batch")
@@ -33,11 +33,12 @@ def create_job_object():
     return body
 
 
-def kube_create_job():
-    api_instance.create_namespaced_job(DEFAULT_NAMESPACE, create_job_object())
+def kube_create_job(iteration):
+    api_instance.create_namespaced_job(
+        DEFAULT_NAMESPACE, create_job_object(iteration))
 
 
-def process_jobs():
+def get_job_duration():
     w = watch.Watch()
     for event in w.stream(api_instance.list_namespaced_job,
                           namespace=DEFAULT_NAMESPACE,
@@ -46,15 +47,20 @@ def process_jobs():
         if job.status.succeeded == NUM_ITERATIONS:
             duration = (job.status.completion_time -
                         job.status.start_time).total_seconds()
-            print(f"Job took {duration} seconds")
             api_instance.delete_namespaced_job(
                 JOB_NAME, DEFAULT_NAMESPACE, propagation_policy="Background")
-            return
+            return duration
+
+
+def tune_resources():
+    for iteration in range(1, 5):
+        kube_create_job(iteration)
+        duration = get_job_duration()
+        print(f"Iteration {iteration} took {duration} seconds")
 
 
 def main():
-    # kube_create_job()
-    process_jobs()
+    tune_resources()
 
 
 if __name__ == "__main__":
